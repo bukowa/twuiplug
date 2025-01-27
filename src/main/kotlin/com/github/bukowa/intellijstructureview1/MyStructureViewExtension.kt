fae1dade1fdb22ab3f3b9c5d04660aebc554667c
Bukowa <gitbukowa@gmail.com>
2025-01-27 14:50:51 +0100

    feat: some working `MyCustomXmlTagTreeElement` for changing display

--- src/main/kotlin/com/github/bukowa/intellijstructureview1/MyStructureViewExtension.kt
+++ src/main/kotlin/com/github/bukowa/intellijstructureview1/MyStructureViewExtension.kt
@@ -1,18 +1,26 @@
 package com.github.bukowa.intellijstructureview1
 
-import com.intellij.ide.structureView.*
+import com.intellij.icons.AllIcons
+import com.intellij.ide.projectView.PresentationData
+import com.intellij.ide.structureView.StructureViewExtension
+import com.intellij.ide.structureView.StructureViewTreeElement
 import com.intellij.ide.structureView.impl.xml.XmlTagTreeElement
-import com.intellij.lang.PsiStructureViewFactory
+import com.intellij.ide.util.treeView.smartTree.TreeElement
+import com.intellij.navigation.ItemPresentation
+import com.intellij.navigation.ItemPresentationProvider
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.command.WriteCommandAction
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiElement
-import com.intellij.psi.PsiFile
+import com.intellij.ide.structureView.xml.XmlStructureViewElementProvider
 import com.intellij.psi.impl.source.tree.CompositeElement
+import com.intellij.psi.impl.source.xml.XmlElementImpl
+import com.intellij.psi.impl.source.xml.XmlTagImpl
 import com.intellij.psi.xml.XmlFile
+import com.intellij.psi.xml.XmlTag
 import com.intellij.util.concurrency.AppExecutorUtil
-import com.jetbrains.rd.util.remove
+import javax.swing.Icon
 
 
 class MyStructureViewExtension : StructureViewExtension {
@@ -32,7 +40,9 @@ class MyStructureViewExtension : StructureViewExtension {
         if (parent != null) {
             project = parent.project
         }
-        return arrayOf() // Return an empty array
+        // return array of custom tree elements with 10 elements
+//        return Array(10) { MyStructureViewTreeElement() }
+        return arrayOf()
     }
 
     override fun getCurrentEditorElement(editor: Editor?, parent: PsiElement?): Any? {
@@ -51,39 +61,108 @@ class MyStructureViewExtension : StructureViewExtension {
         // Function to recursively filter children and collect elements to remove
         fun filterChildElements(root: XmlTagTreeElement) {
             // Use an iterator to safely remove elements while iterating
-            AppExecutorUtil.getAppExecutorService().submit {
-                ApplicationManager.getApplication().runReadAction {
+            val iterator = root.children.iterator()
 
-                    val iterator = root.children.iterator()
+            while (iterator.hasNext()) {
+                val element = iterator.next()
 
-                    while (iterator.hasNext()) {
-                        val element = iterator.next()
-
-                        if (element is XmlTagTreeElement) {
-                            val key = element.key
-                            if (key == "XmlTag:yes" || key == "XmlTag:no") {
-                                println("Removing child with key: $key")
-                                AppExecutorUtil.getAppExecutorService().submit {
-                                    WriteCommandAction.runWriteCommandAction(project) {
-                                        (root.element as CompositeElement).removeChild(element.element!!.node)
-                                    }
-                                }
-                            } else {
-                                // Recursively filter the children if not removed
-                                filterChildElements(element) // Pass mutable list for safe removal
-                            }
-                        }
-                    }
-                }
-                // Filter the baseChildren directly and remove unwanted elements
-                val iterator = baseChildren.iterator()
-                while (iterator.hasNext()) {
-                    val element = iterator.next()
-                    if (element is XmlTagTreeElement) {
-                        filterChildElements(element)
+                if (element is XmlTagTreeElement) {
+                    val key = element.key
+                    if (key == "XmlTag:yes" || key == "XmlTag:no") {
+                        println("Removing child with key: $key")
+                    } else if (element.children.size > 0) {
+                        // Recursively filter the children if not removed
+                        filterChildElements(element) // Pass mutable list for safe removal
                     }
                 }
             }
         }
+
+        // Filter the baseChildren directly and remove unwanted elements
+        AppExecutorUtil.getAppExecutorService().submit {
+            WriteCommandAction.runWriteCommandAction(project) {
+                val iterator = baseChildren.iterator()
+
+                while (iterator.hasNext()) {
+                    val element = iterator.next()
+
+                    if (element is XmlTagTreeElement) {
+                        filterChildElements(element)
+                    }
+
+                }
+            }
+
+        }
+        // Call the super method if needed
+//    super.filterChildren(baseChildren, extensionChildren)
     }
+}
+
+class MyStructureViewTreeElement : StructureViewTreeElement {
+    override fun getPresentation(): ItemPresentation {
+        return MyItemPresentation()
+    }
+
+    override fun getChildren(): Array<TreeElement> {
+        return arrayOf()
+    }
+
+    override fun getValue(): Any {
+        return this
+    }
+
+}
+
+class MyItemPresentation : ItemPresentation {
+    override fun getIcon(unused: Boolean) = AllIcons.Nodes.Test
+    override fun getPresentableText(): String {
+        return ""
+    }
+}
+
+class MyXmlTagImpl : ItemPresentationProvider<XmlTagImpl> {
+
+    override fun getPresentation(item: XmlTagImpl): ItemPresentation {
+
+        return PresentationData(
+            "asd",
+            item.containingFile.name,
+            item.getIcon(0),
+            null
+        )
+    }
+}
+
+class MyCustomXmlStructureViewElementProvider : XmlStructureViewElementProvider {
+    override fun createCustomXmlTagTreeElement(p0: XmlTag): StructureViewTreeElement? {
+        // Apply custom logic for specific tags
+        if (p0.name == "uientry") {
+            return MyCustomXmlTagTreeElement(p0)
+        }
+        // Return null for default handling of other tags
+        return null
+    }
+}
+
+class MyCustomXmlTagTreeElement(private val xmlTag: XmlTag) : StructureViewTreeElement {
+    override fun getValue(): Any = xmlTag
+
+    override fun getPresentation(): ItemPresentation {
+        return object : ItemPresentation {
+            override fun getPresentableText(): String {
+                // Customize how the tag appears in the Structure View
+                return "<${xmlTag.name}> (i added this shit)"
+            }
+
+            override fun getLocationString(): String? = xmlTag.containingFile.name
+            override fun getIcon(unused: Boolean): Icon? = null // Use a custom icon if desired
+        }
+    }
+
+    override fun getChildren(): Array<StructureViewTreeElement> {
+        // Recursively process children if needed
+        return xmlTag.subTags.map { MyCustomXmlTagTreeElement(it) }.toTypedArray()
+    }
+
 }
