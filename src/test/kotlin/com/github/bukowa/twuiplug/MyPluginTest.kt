8751ea560c170a592efaf02b43d074ca12084de8
Bukowa <gitbukowa@gmail.com>
2025-01-28 19:17:26 +0100

    tests: improve file quality log

--- src/test/kotlin/com/github/bukowa/twuiplug/MyPluginTest.kt
+++ src/test/kotlin/com/github/bukowa/twuiplug/MyPluginTest.kt
@@ -60,11 +60,43 @@ class MyPluginTest : BasePlatformTestCase() {
         assert(areFilesEqual(binary_from_xml, original_binary_file))
     }
 
+
     fun areFilesEqual(file1: File, file2: File): Boolean {
-        if (file1.length() != file2.length()) {
-            return false  // Files are different if their lengths don't match
+        val file1Size = file1.length()
+        val file2Size = file2.length()
+
+        // Check file sizes first
+        if (file1Size != file2Size) {
+            println("File sizes are different:")
+            println("- ${file1.absolutePath}: $file1Size bytes")
+            println("- ${file2.absolutePath}: $file2Size bytes")
+            return false
         }
 
-        return file1.readBytes().contentEquals(file2.readBytes())
+        // Read file contents
+        val file1Bytes = file1.readBytes()
+        val file2Bytes = file2.readBytes()
+
+        // Compare byte by byte and log differences
+        for (i in file1Bytes.indices) {
+            if (file1Bytes[i] != file2Bytes[i]) {
+                println("Files differ at byte $i:")
+                println("- File 1: ${file1Bytes[i]} (${file1.absolutePath})")
+                println("- File 2: ${file2Bytes[i]} (${file2.absolutePath})")
+                return false
+            }
+        }
+
+        println("Files are equal: ${file1.absolutePath} and ${file2.absolutePath}")
+        return true
     }
+
+    fun assertFilesEqual(xmlFromBinary: File, originalXmlFile: File) {
+        require(areFilesEqual(xmlFromBinary, originalXmlFile)) {
+            "Files are not equal:\n" +
+                    "- File 1: ${xmlFromBinary.absolutePath} (size: ${xmlFromBinary.length()} bytes)\n" +
+                    "- File 2: ${originalXmlFile.absolutePath} (size: ${originalXmlFile.length()} bytes)"
+        }
+    }
+
 }
