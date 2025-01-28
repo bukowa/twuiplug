80575fac30ae48e350fc1b43b0df49fca71b4d1e
Bukowa <gitbukowa@gmail.com>
2025-01-28 14:41:35 +0100

    tests: add proper tests

--- src/test/kotlin/com/github/bukowa/intellijstructureview1/MyPluginTest.kt
+++ src/test/kotlin/com/github/bukowa/intellijstructureview1/MyPluginTest.kt
@@ -8,49 +8,68 @@ import com.intellij.testFramework.fixtures.BasePlatformTestCase
 import com.intellij.util.PsiErrorElementUtil
 import com.github.bukowa.intellijstructureview1.services.MyProjectService
 import com.github.bukowa.intellijstructureview1.utils.RubyScriptExecutor
-import com.intellij.testFramework.TestDataFile
-import org.jruby.embed.ScriptingContainer
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.openapi.vfs.VirtualFileManager
+import com.intellij.testFramework.PsiTestUtil.addContentRoot
 import java.io.File
 import java.nio.file.Paths
-import java.time.Instant
 
 @TestDataPath("\$CONTENT_ROOT/testdata")
 class MyPluginTest : BasePlatformTestCase() {
 
-    fun testXMLFile() {
-        val psiFile = myFixture.configureByText(XmlFileType.INSTANCE, "<foo>bar</foo>")
-        val xmlFile = assertInstanceOf(psiFile, XmlFile::class.java)
-
-        assertFalse(PsiErrorElementUtil.hasErrors(project, xmlFile.virtualFile))
-        assertNotNull(xmlFile.rootTag)
-
-        xmlFile.rootTag?.let {
-            assertEquals("foo", it.name)
-            assertEquals("bar", it.value.text)
-        }
-    }
-
-    fun testRename() {
-        myFixture.testRename("foo.xml", "foo_after.xml", "a2")
-    }
-
     fun testProjectService() {
         val projectService = project.service<MyProjectService>()
-
         assertNotSame(projectService.getRandomNumber(), projectService.getRandomNumber())
     }
 
-    fun testJRuby() {
-//        myFixture.copyFileToProject("options_mods.xml")
-        RubyScriptExecutor.executeLoadUi2XmlScript("${testDataPath}/options_mods", "${testDataPath}/options_mods.xml")
-    }
     override fun getTestDataPath(): String {
         return Paths.get("src/test/testdata").toAbsolutePath().normalize().toString()
     }
 
-    fun testJRuby2() {
+    fun writeVirtualFileToTempFile(virtualFile: VirtualFile): File {
+        require(virtualFile.isValid) { "The virtual file is not valid." }
 
-//        myFixture.copyFileToProject("options_mods.xml")
-        RubyScriptExecutor.executeLoadXmlToUiScript("${testDataPath}\\options_mods.xml", "${testDataPath}\\options_mods")
+        // Create a temporary file
+        val tempFile = File.createTempFile("virtual_file", ".tmp")
+        tempFile.deleteOnExit() // Ensure the temp file is deleted when the program exits
+
+        // Write the content of the VirtualFile to the temp file
+        virtualFile.inputStream.use { input ->
+            tempFile.outputStream().use { output ->
+                input.copyTo(output)
+            }
+        }
+
+        println("Virtual file written to: ${tempFile.absolutePath}")
+        return tempFile
+    }
+
+    fun getNewTestFile(path: String): File {
+        return writeVirtualFileToTempFile(myFixture.copyFileToProject(path))
+    }
+
+    fun test_rubyScriptExecution() {
+        val original_binary_file = getNewTestFile("options_mods")
+        val original_xml_file = getNewTestFile("options_mods.xml")
+
+        if (original_binary_file.length() < 1024 || original_xml_file.length() < 1024) {
+            throw Exception("invalid files")
+        }
+
+        val xml_from_binary = File.createTempFile("file", "")
+        RubyScriptExecutor.executeUi2XmlScript(original_binary_file.path, xml_from_binary.path)
+        assert(areFilesEqual(xml_from_binary, original_xml_file))
+
+        val binary_from_xml = File.createTempFile("file2", "")
+        RubyScriptExecutor.executeXml2UiScript(xml_from_binary.path, binary_from_xml.path)
+        assert(areFilesEqual(binary_from_xml, original_binary_file))
+    }
+
+    fun areFilesEqual(file1: File, file2: File): Boolean {
+        if (file1.length() != file2.length()) {
+            return false  // Files are different if their lengths don't match
+        }
+
+        return file1.readBytes().contentEquals(file2.readBytes())
     }
 }
